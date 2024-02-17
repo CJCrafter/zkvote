492eb273b3c42681a410a39a991ad9ea2ba8d035
s-alad <n7.saad@gmail.com>
2024-02-17 15:29:33 -0500

    add verify vheck

---
+++ zk/app/utils/verify.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const { groth16 } = require("snarkjs");
+const appRoot = require('app-root-path');
+
+async function verify(proof, publicSignals) {
+
+    const vKey = JSON.parse(fs.readFileSync(`${appRoot}/circuits/build/keys/verification_key.json`));
+    const res = await groth16.verify(vKey, publicSignals, proof);
+
+    if (res === true) {
+        console.log("Verification OK");
+        return true;
+    } else {
+        console.log("Invalid proof");
+        return false;
+    }
+}
+
+module.exports = { verify };
