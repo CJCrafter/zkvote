8cd90cd425cd51cac1b2e2b1d80d2a6684e2d4d6
Collin Barber <collinjbarber@gmail.com>
2024-02-17 10:07:33 -0500

    multi-module project

---
+++ web/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="m-5">
+            <div className="m-2 border-2 bg-blue-300">
+                <p className="p-2 text-black text-center text-2xl">Election Chain</p>
+
+            </div>
+
+            {/* Big panel buttons for registering and voting */}
+            <div>
+
+            </div>
+        </div>
+    );
+}
---
+++ web/app/util/panelbuttonlist.tsx
@@ -0,0 +1,5 @@
+export default function PanelButton(
+    {href, text, description}: { href: string, text: string, description: string }
+) {
+    return <></>
+}
