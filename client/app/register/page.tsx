81bb2d9f75ff2d82c8e6d71176b6cb30039a4548
s-alad <n7.saad@gmail.com>
2024-02-17 18:57:00 -0500

    new

--- client/app/register/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function RegisterPage() {
-    return (
-        <div>
-            
-        </div>
-    );
-}
--- client/app/util/iconbutton.tsx
+++
@@ -1,5 +0,0 @@
-export default function IconButton({href, name, logo}: {href: string, name: string, logo: string} ) {
-    return <a href={href} className="text-blue-600 underline hover:text-gray-600">
-        {name} <img src={logo} alt={name} className="w-6 h-6 inline" />
-    </a>
-}
