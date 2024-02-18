5ea81d1b845e3c97159cbf4af2d89ee294ada743
s-alad <n7.saad@gmail.com>
2024-02-17 23:24:29 -0500

    add two emtpy lines

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -150,6 +150,8 @@ export default function Home() {
                             </div>
                     ) : ''
                 }
+
+                
             </div>
         </main>
     );
