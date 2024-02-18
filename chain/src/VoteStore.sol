9e14f24b8cefbcc0d081af4e8322e3e84f347e96
Wes <wjorgensen29@gmail.com>
2024-02-17 20:24:26 -0500

    Changed logo, modifying vote store

--- chain/src/VoteStore.sol
+++ chain/src/VoteStore.sol
@@ -5,10 +5,22 @@ contract VoteStore {
     uint256[] private s_spentTickets;
     uint256 private s_votesForA;
     uint256 private s_votesForB;
+    Candidate[] private s_candidates;
 
     mapping(uint256 => bool) private spentTicketMapping;
+    mapping(string => uint256) internal nameToArrayIndex;
 
     error VoterAlreadyVoted();
+
+    struct Candidate {
+        string name;
+        uint256 votes;
+    }
+
+    constructor(){
+        //So the name to array mapping works
+        s_candidates.push(Candidate("",0));
+    }
 
     modifier notVoted(uint256 _spentTicket) {
         if(spentTicketMapping[_spentTicket]){
@@ -21,11 +33,12 @@ contract VoteStore {
         return spentTicketMapping[_spentTicket];
     }
 
-    function vote(uint256 _ticket, bool _votedFor) public notVoted(_ticket) {
-        if(_votedFor){
-            s_votesForA++;
-        } else {
-            s_votesForB++;
+    function vote(uint256 _ticket, string calldata _name) public notVoted(_ticket) {
+        if(nameToArrayIndex[_name] == 0){
+            s_candidates.push(Candidate(_name, 1));
+        }
+        else{
+            ++s_candidates[nameToArrayIndex[_name]].votes;
         }
 
         s_spentTickets.push(_ticket);
@@ -36,7 +49,11 @@ contract VoteStore {
         return s_spentTickets.length;
     }
 
-    function getResult() public view returns (uint256, uint256) {
-        return (s_votesForA, s_votesForB);
+    function getResult() public view returns () {
+        
+    }
+
+    function getNumberOfCandidates() public view returns (uint256) {
+        return s_candidates.length - 1;
     }
 }
